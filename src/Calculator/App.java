3bfcd88ac83f92bd78a1b9c592bd03d0031a8009
Student <JonCook7@ProtonMail.com>
2023-01-17 16:10:14 -0800

    Simple main method

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -1,15 +1,25 @@
 package Calculator;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Lexer lexer = new Lexer();
+        Parser parser = new Parser();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a mathematical expression:");
 
+        
         while(true) {
+            System.out.println("Enter a mathematical expression:");
             String input = sc.nextLine();
 
-            
+            try {
+                List<Token> tokens = lexer.tokenize(input);
+                double result = parser.evaluate(tokens);
+                System.out.println("Result: " + result);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
 
             if (input.equals("quit")) {
                 break;
